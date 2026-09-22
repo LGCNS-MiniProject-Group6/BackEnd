8a35ea9801c9c6a7cc4076c43625b17e700c2818
gwakgwaklee <ahalgrgar1@naver.com>
2026-09-22 11:48:34 +0900

    feat: 인증 API 필수 기능 구현 및 테스트 완료

--- src/main/java/com/miniproject1/miniproject1/auth/service/signup/EmailCheckService.java
+++ src/main/java/com/miniproject1/miniproject1/auth/service/signup/EmailCheckService.java
@@ -1,6 +1,7 @@
 package com.miniproject1.miniproject1.auth.service.signup;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.miniproject1.miniproject1.auth.dto.signup.response.EmailCheckResponseDTO;
 import com.miniproject1.miniproject1.user.repository.UserRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EmailCheckService {
 
     private final UserRepository userRepository;
--- src/main/java/com/miniproject1/miniproject1/commons/config/SecurityConfig.java
+++ src/main/java/com/miniproject1/miniproject1/commons/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/auth/**",
+                                "/api/signup/**",
+
                                 "/error")
                         .permitAll()
                         .anyRequest().permitAll() // 개발 초기 전체 허용
